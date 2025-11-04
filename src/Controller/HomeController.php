a09ca3161048ae5d7aad44d74186978f3d29adca
Nofiniaina <nofyrnd@gmail.com>
2025-11-04 09:48:50 +0300

    add category and subcategory

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/home', name: 'home')]
+    public function index(CategoryRepository $categoryRepo): Response
     {
+        $categories = $categoryRepo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $categories,
         ]);
     }
 }
---
+++ src/DataFixtures/CategorySubCategoryFixtures.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\SubCategory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategorySubCategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $categories = [
+            'Smartphones & Tablets' => [
+                'Smartphones', 'Tablets', 'Accessories'
+            ],
+            'Computers & Laptops' => [
+                'Laptops', 'Desktops', 'Computer Components', 'Accessories'
+            ],
+            'Gaming' => [
+                'Consoles', 'Games', 'Gaming Accessories'
+            ],
+            'Audio & Music' => [
+                'Headphones', 'Speakers', 'Microphones'
+            ],
+            'Smart Home & IoT' => [
+                'Smart Assistants', 'Smart Lighting', 'Security', 'Home Automation'
+            ],
+            'Wearables' => [
+                'Smartwatches', 'Fitness Trackers', 'VR & AR Devices'
+            ],
+            'Photography & Video' => [
+                'Cameras', 'Lenses', 'Accessories'
+            ],
+            'Networking & Storage' => [
+                'Routers & Wi-Fi', 'External Storage', 'NAS & Servers'
+            ],
+            'Office Equipment' => [
+                'Printers & Scanners', 'Projectors', 'Office Accessories'
+            ],
+            'Tech Gadgets & Miscellaneous' => [
+                'Drones', 'Action Cameras', 'E-readers', 'Tech Toys'
+            ]
+        ];
+
+        foreach ($categories as $categoryName => $subCategoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+
+            foreach ($subCategoryName as $subName) {
+                $subCategory = new SubCategory();
+                $subCategory->setName($subName);
+                $category->addSubCategory($subCategory);
+            }
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
