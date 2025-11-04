d02d2d989113a46a05b0a93e728730b8a0b0d821
Nofiniaina <nofyrnd@gmail.com>
2025-11-04 14:18:09 +0300

    add AdminDashboardController

---
+++ src/Controller/Admin/AdminDashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminDashboardController extends AbstractController
+{
+    #[Route('/admin/dashboard', name: 'admin.dashboard')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig', [
+            'controller_name' => 'AdminDashboardController',
+        ]);
+    }
+}
